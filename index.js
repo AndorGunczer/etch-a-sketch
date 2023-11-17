c332c9845a26a72e1764db5ccda7f94047c44709
Andor Gunczer <andor.gunczer@gmail.com>
2023-11-17 18:43:13 +0100

    add eraser and color button

--- index.js
+++ index.js
@@ -5,14 +5,36 @@ for (let i = 0; i < 6; i++) {
         const draw_grid = document.createElement('div');
         draw_grid.setAttribute('class', 'draw_grid');
         container.appendChild(draw_grid);
-    }
-    
-}
+    };
+};
 
 const grid = document.querySelectorAll('.draw_grid');
+const eraser = document.getElementById('eraser');
+const color = document.getElementById('color');
 
-grid.forEach(element => {
-    element.addEventListener('mouseover', () => {
-        element.setAttribute('style', 'background-color: red;');
+eraser.addEventListener('click', draw.bind(null, "white"));
+color.addEventListener('click', draw.bind(null, "red"));
+
+function colorFunctionCaller() {
+    // colorFunction()
+}
+
+function colorFunction(element, color) {
+    console.log(element);
+    element.setAttribute('style', "background-color: " + color + ";");
+};
+
+function draw(color) {
+    grid.forEach(element => {
+        element.removeEventListener('mouseover', colorFunction.bind(null, element, color));
+        element.addEventListener('mouseover', colorFunction.bind(null, element, color));
     });
-});
+};
+
+// function draw(color) {
+//     grid.forEach(element => {
+//         element.addEventListener('mouseover', () => {
+//             element.setAttribute('style', "background-color: red;");
+//         });
+//     });
+// };
