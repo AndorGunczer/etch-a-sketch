c050c2491dc130cd35d31df17e4bdde56cc8aaa2
Andor Gunczer <andor.gunczer@gmail.com>
2023-11-18 14:25:37 +0100

    Add responsive slider, move the process of grid creation into its own function for reusability and to avoid code repetition

--- index.js
+++ index.js
@@ -5,16 +5,26 @@ let square_per_row = slider_value / 2;
 let square_per_column = square_per_row;
 let square_size = 32 / square_per_row;
 
-for (let i = 0; i < square_per_column; i++) {
-    for (let j = 0; j < square_per_row; j++) {
-        const draw_grid = document.createElement('div');
-        draw_grid.setAttribute('class', 'draw_grid');
-        draw_grid.setAttribute('style', `height: ${square_size}em; width: ${square_size}em;`);
-        container.appendChild(draw_grid);
-    };
+create_grid();
+
+slider.onchange = () => {
+
+    const draw_grid = Array.from(document.getElementsByClassName('draw_grid'));
+    let x = 0;
+    slider_value = slider.value;
+    square_per_row = slider_value / 2;
+    square_per_column = square_per_row;
+    square_size = 32 / square_per_row;
+
+    console.log(slider.value);
+    console.log(square_per_column);
+    console.log(square_size);
+    console.log(draw_grid);
+    
+    remove_grid(draw_grid);
+    create_grid();
 };
 
-const grid = document.querySelectorAll('.draw_grid');
 const eraser = document.getElementById('eraser');
 const color = document.getElementById('color');
 
@@ -23,15 +33,17 @@ color.addEventListener('click', draw);
 
 function colorFunction() {
     console.log("drawer called");
-    this.setAttribute('style', "background-color: " + "red" + ";");
+    this.setAttribute('style', `height: ${square_size}em; width: ${square_size}em; background-color: red;`);
 };
 
 function eraseFunction() {
     console.log("eraser called");
-    this.setAttribute('style', "background-color: " + "white" + ";");
+    this.setAttribute('style', `height: ${square_size}em; width: ${square_size}em; background-color: white;`);
 };
 
 function draw() {
+    const grid = document.querySelectorAll('.draw_grid');
+
     grid.forEach(element => {
         element.removeEventListener('mouseover', eraseFunction);
         element.addEventListener('mouseover', colorFunction);
@@ -39,8 +51,28 @@ function draw() {
 };
 
 function erase() {
+    const grid = document.querySelectorAll('.draw_grid');
+
     grid.forEach(element => {
         element.removeEventListener("mouseover", colorFunction);
         element.addEventListener("mouseover", eraseFunction);
+    });
+};
+
+function create_grid() {
+    for (let i = 0; i < square_per_column; i++) {
+        for (let j = 0; j < square_per_row; j++) {
+
+            draw_grid = document.createElement('div');
+            draw_grid.setAttribute('class', 'draw_grid');
+            draw_grid.setAttribute('style', `height: ${square_size}em; width: ${square_size}em;`);
+            container.appendChild(draw_grid);
+        };
+    };
+};
+
+function remove_grid(grid_to_remove) {
+    grid_to_remove.forEach(element => {
+        element.remove();
     });
 };
