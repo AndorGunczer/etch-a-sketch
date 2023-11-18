0d4244d3b229778312cf733ccefff8562e6f4676
Andor Gunczer <andor.gunczer@gmail.com>
2023-11-18 13:46:08 +0100

    add grid calculation based on slider value (slider value = square per row or square per column)

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const container = document.getElementsByClassName('container')[0];
+const slider = document.getElementById("my_range");
+let slider_value = slider.value;
+let square_per_row = slider_value / 2;
+let square_per_column = square_per_row;
+let square_size = 32 / square_per_row;
 
-for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < 6; j++) {
+for (let i = 0; i < square_per_column; i++) {
+    for (let j = 0; j < square_per_row; j++) {
         const draw_grid = document.createElement('div');
         draw_grid.setAttribute('class', 'draw_grid');
+        draw_grid.setAttribute('style', `height: ${square_size}em; width: ${square_size}em;`);
         container.appendChild(draw_grid);
     };
 };
@@ -23,7 +29,7 @@ function colorFunction() {
 function eraseFunction() {
     console.log("eraser called");
     this.setAttribute('style', "background-color: " + "white" + ";");
-}
+};
 
 function draw() {
     grid.forEach(element => {
@@ -36,5 +42,5 @@ function erase() {
     grid.forEach(element => {
         element.removeEventListener("mouseover", colorFunction);
         element.addEventListener("mouseover", eraseFunction);
-    })
-}
+    });
+};
